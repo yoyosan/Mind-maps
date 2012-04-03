36895299c636b52f404e6abbe1733f82e636dbdb
Marius Palade <veselu@gmail.com>
2012-04-03 17:29:24 +0300

    Moved item to proper directory.

---
+++ Code-Complete-2nd-edition_Steve_McConnell/Code-Complete.mm
@@ -0,0 +1,308 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1333436264795" ID="ID_1452473876" MODIFIED="1333462507251">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Code Complete
+    </p>
+    <p style="text-align: center">
+      2nd edition
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1333437823661" ID="ID_1038722867" MODIFIED="1333438975085" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Chapter 1
+    </p>
+    <p style="text-align: center">
+      Software Construction
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="full-1"/>
+<node CREATED="1333437936627" ID="ID_1513114132" MODIFIED="1333438951424">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      What is software
+    </p>
+    <p>
+      construction?
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1333437971938" ID="ID_1853268561" MODIFIED="1333438592275" TEXT="refers to the process of building" VSHIFT="-10"/>
+<node CREATED="1333438015538" HGAP="24" ID="ID_599760827" MODIFIED="1333438595867" TEXT="hands-on part of creating something" VSHIFT="-1"/>
+<node CREATED="1333438081488" FOLDED="true" ID="ID_590621633" MODIFIED="1333445769065">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Software development comprises of
+    </p>
+    <p style="text-align: center">
+      the following activities
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1333438102176" ID="ID_213510116" MODIFIED="1333438108309" TEXT="Problem definition"/>
+<node CREATED="1333438109072" ID="ID_1037806852" MODIFIED="1333438117693" TEXT="Requirements development"/>
+<node CREATED="1333438118592" ID="ID_899936593" MODIFIED="1333438123605" TEXT="Construction planning"/>
+<node CREATED="1333438124448" ID="ID_60527583" MODIFIED="1333438138933" TEXT="Software architecture or high level design"/>
+<node CREATED="1333438140016" ID="ID_401303951" MODIFIED="1333438143861" TEXT="Detailed design"/>
+<node CREATED="1333438144525" ID="ID_117277046" MODIFIED="1333438151573" TEXT="Coding and debugging"/>
+<node CREATED="1333438152255" ID="ID_887353068" MODIFIED="1333438156933" TEXT="Unit testing"/>
+<node CREATED="1333438157599" ID="ID_450657320" MODIFIED="1333438163861" TEXT="Integration testing"/>
+<node CREATED="1333438164511" ID="ID_818233777" MODIFIED="1333438170197" TEXT="Integration"/>
+<node CREATED="1333438177015" ID="ID_881293434" MODIFIED="1333438179668" TEXT="System testing"/>
+<node CREATED="1333438180471" ID="ID_127811294" MODIFIED="1333438184964" TEXT="Corrective maintenance"/>
+</node>
+<node CREATED="1333438402147" FOLDED="true" ID="ID_1524528622" MODIFIED="1333445769921" VSHIFT="4">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Specific tasks involved
+    </p>
+    <p style="text-align: center">
+      in construction
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1333438418099" ID="ID_378321248" MODIFIED="1333438456888">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Verifying that the groundwork has been laid
+    </p>
+    <p style="text-align: center">
+      so that can proceed successfully
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1333438471618" ID="ID_402882547" MODIFIED="1333438481252" TEXT="Determining how your code will be tested"/>
+<node CREATED="1333438486410" ID="ID_1731097971" MODIFIED="1333438501351" TEXT="Designing and writing classes and routines"/>
+<node CREATED="1333438529761" ID="ID_1412167368" MODIFIED="1333438544039" TEXT="Creating and naming variables and constants"/>
+<node CREATED="1333438544857" ID="ID_1019624429" MODIFIED="1333438723533">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: left">
+      Selecting control structures and organizing
+    </p>
+    <p style="text-align: left">
+      block statements
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1333438600976" ID="ID_1529767981" MODIFIED="1333438718812">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: left">
+      Unit testing, integration testing and
+    </p>
+    <p style="text-align: left">
+      debugging your own code
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1333438679495" ID="ID_305457174" MODIFIED="1333438728092">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: left">
+      Reviewing other team members low-level designs
+    </p>
+    <p style="text-align: left">
+      and code and having them review yours
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1333438734630" ID="ID_385534835" MODIFIED="1333438822210">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Polish code by carefully formatting
+    </p>
+    <p>
+      and commenting it
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1333438770933" ID="ID_173444225" MODIFIED="1333438809442">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Integrated software components that
+    </p>
+    <p>
+      were created separately
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1333438824861" ID="ID_1206454225" MODIFIED="1333438856850">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tuning code to make it faster and
+    </p>
+    <p>
+      use fewer resources
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1333438920451" FOLDED="true" ID="ID_21829059" MODIFIED="1333445771472" VSHIFT="15">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Why is software
+    </p>
+    <p>
+      construction important?
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1333439137303" ID="ID_1041362035" MODIFIED="1333439287723">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>is a large part of softwarer development</b>
+    </p>
+    <p>
+      it usually takes 30 - 80% of the total
+    </p>
+    <p>
+      time spent on a project, though determining
+    </p>
+    <p>
+      its success.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1333439289589" ID="ID_305957961" MODIFIED="1333439553583">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>is the central activity in software development </b>
+    </p>
+    <p>
+      follows after setting requirements and architecture,
+    </p>
+    <p>
+      precedes sytem testing though being the center
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1333439555609" ID="ID_12579909" MODIFIED="1333439660309">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>focusing on&#160;construction, the individual programmer's </b>
+    </p>
+    <p>
+      <b>productivity can improve enormously </b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1333439662007" ID="ID_1981074932" MODIFIED="1333439967152">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Source code, the product of construction is often </b>
+    </p>
+    <p>
+      <b>the only accurate description of the software </b>
+    </p>
+    <p>
+      requirements specifications and design documents can
+    </p>
+    <p>
+      go out of date, but the source code is always updated.
+    </p>
+    <p>
+      That's why the source code should be of the highest
+    </p>
+    <p>
+      quality possible.
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1333440202334" ID="ID_734752043" MODIFIED="1333440225883">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>it's the only activity that's guaranteed to be done </b>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</map>
