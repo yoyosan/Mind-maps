d76c6caf9671a1e1bed664d7ea65d6ace231e667
Marius Palade <marius.palade@1and1.ro>
2013-11-08 15:55:01 +0200

    Add Symfony 2 The Book to the stack.

---
+++ Symfony2-The-Book/Symfony2-Book.mm
@@ -0,0 +1,21 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1383918386810" ID="ID_500154351" MODIFIED="1383918404225" TEXT="Symfony 2">
+<node CREATED="1383918408604" ID="ID_1039446326" MODIFIED="1383918439995" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Creating and using
+    </p>
+    <p>
+      Templates
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</map>
