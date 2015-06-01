9d826a393f59b1640110237c74ce3a61853f35f9
Yoyosan <veselu@gmail.com>
2015-06-01 18:25:53 +0300

    Add pair programming mindmap.

---
+++ Craftsmanhip/Pair-programming/Pair-programming.mm
@@ -0,0 +1,395 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1433158584714" ID="ID_1424371804" LINK="http://sundeepgupta.ca/breaking-down-successful-pair-programming/" MODIFIED="1433164807476" TEXT="Pair programming">
+<font NAME="Source Code Pro" SIZE="12"/>
+<node CREATED="1433158734233" HGAP="28" ID="ID_933153335" MODIFIED="1433167447849" POSITION="right" TEXT="info" VSHIFT="-26">
+<font NAME="Source Code Pro" SIZE="12"/>
+<node CREATED="1433167052090" ID="ID_677513559" MODIFIED="1433167349834" TEXT="brought by the extreme programming movement"/>
+<node CREATED="1433167081280" ID="ID_491449823" MODIFIED="1433167136907" TEXT="you meet wonderful people">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1433167162919" ID="ID_465822677" MODIFIED="1433167339396" TEXT="&quot;it depends&quot; whether it&apos;s worth it">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1433167686067" ID="ID_200635423" MODIFIED="1433167743346">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hope that the business(and the individuals)&#160;will
+    </p>
+    <p>
+      benefit more than if the two worked independently.
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1433167223599" HGAP="47" ID="ID_864752843" MODIFIED="1433167453132" POSITION="left" VSHIFT="-393">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <img src="mgDJe9w.png" />
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Pair programming success matrix
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1433167563700" ID="ID_1474861339" MODIFIED="1433167616219">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      watch the gap&#160;in effectiveness
+    </p>
+    <p>
+      between the two individuals
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1433168584397" ID="ID_398851456" MODIFIED="1433168586328" POSITION="right" TEXT="pairs">
+<node CREATED="1433168587229" ID="ID_1691264034" MODIFIED="1433169863842">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      with small effectivness gap
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-1"/>
+<node CREATED="1433168638820" ID="ID_1642389375" MODIFIED="1433168839917">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      work out better
+    </p>
+    <p>
+      due to
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1433168805299" ID="ID_634949473" MODIFIED="1433168812207" TEXT="good chemistry">
+<node CREATED="1433168854611" ID="ID_1506149665" MODIFIED="1433168860607" TEXT="understand and respect each other"/>
+<node CREATED="1433168878067" ID="ID_1903440353" MODIFIED="1433168881558" TEXT="communicate often"/>
+<node CREATED="1433168886362" ID="ID_1345761010" MODIFIED="1433168890030" TEXT="help each other learn"/>
+</node>
+<node CREATED="1433168813475" ID="ID_1862770408" MODIFIED="1433168829143" TEXT="devs have more in common"/>
+</node>
+</node>
+<node CREATED="1433168908746" ID="ID_1386987497" MODIFIED="1433168917938" TEXT="junior pairs">
+<icon BUILTIN="full-2"/>
+<node CREATED="1433168925530" ID="ID_1553117613" MODIFIED="1433168935190" TEXT="more beneficial than senior pairs"/>
+<node CREATED="1433168940442" ID="ID_798005053" MODIFIED="1433168943326" TEXT="why?">
+<node CREATED="1433169060217" ID="ID_1305556574" MODIFIED="1433169066629" TEXT="more empathy to one another"/>
+<node CREATED="1433168960858" ID="ID_612789665" MODIFIED="1433168971134" TEXT="devs have weak opinions"/>
+<node CREATED="1433168976418" ID="ID_1829577426" MODIFIED="1433168979134" TEXT="egos are smaller"/>
+<node CREATED="1433168984738" ID="ID_542465413" MODIFIED="1433168994077" TEXT="devs care more about learning than being &quot;right&quot;"/>
+<node CREATED="1433169006802" ID="ID_364660431" MODIFIED="1433169015358" TEXT="new kids in school effect"/>
+</node>
+<node CREATED="1433169093361" ID="ID_379256895" MODIFIED="1433169113807">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Primary goal: <b>Learn</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1433169076337" ID="ID_1977618543" MODIFIED="1433169082511" TEXT="senior pairs">
+<icon BUILTIN="full-3"/>
+<node CREATED="1433169162921" ID="ID_1917736402" MODIFIED="1433169167060" TEXT="harder to get a good match"/>
+<node CREATED="1433169603525" ID="ID_1154322097" MODIFIED="1433169726912" TEXT="the good">
+<node CREATED="1433169753972" ID="ID_626144724" MODIFIED="1433169758664" TEXT="devs respect each other"/>
+<node CREATED="1433169759644" ID="ID_1350558751" MODIFIED="1433169769552" TEXT="talk a lot">
+<node CREATED="1433169777573" ID="ID_900694511" MODIFIED="1433169779680" TEXT="debate"/>
+<node CREATED="1433169780420" ID="ID_1860091439" MODIFIED="1433169782232" TEXT="discuss"/>
+<node CREATED="1433169782980" ID="ID_712523874" MODIFIED="1433169784920" TEXT="plan"/>
+</node>
+</node>
+<node CREATED="1433169178800" ID="ID_1035591843" MODIFIED="1433169721010">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      the ugly
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1433169184992" ID="ID_1008026864" MODIFIED="1433169196100" TEXT="devs are different"/>
+<node CREATED="1433169208368" ID="ID_541513195" MODIFIED="1433169214004" TEXT="devs have different opinions"/>
+<node CREATED="1433169225528" ID="ID_1265924799" MODIFIED="1433169237122" TEXT="more experience, stronger opinions">
+<font BOLD="true" NAME="Source Code Pro" SIZE="12"/>
+</node>
+<node CREATED="1433169315239" ID="ID_648389822" MODIFIED="1433169333299" TEXT="more experience, more ego"/>
+<node CREATED="1433169335279" ID="ID_1378657108" MODIFIED="1433169424381">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      if one dev has a large ego, the pair
+    </p>
+    <p>
+      may wield poor decisions <b>due to</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1433169362751" ID="ID_1591190408" MODIFIED="1433169429251" TEXT="being insecure"/>
+<node CREATED="1433169430247" ID="ID_1827249089" MODIFIED="1433169458666" TEXT="&quot;tiptoeing&quot; to not hurt the other person&apos;s ego"/>
+</node>
+<node CREATED="1433169480734" ID="ID_181404585" MODIFIED="1433169516670" TEXT="Both may have large egos">
+<icon BUILTIN="stop-sign"/>
+<node CREATED="1433169523462" ID="ID_362065650" MODIFIED="1433169545389">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      they argue, not debate&#160;<b>a lot</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1433169556029" ID="ID_993059868" MODIFIED="1433169564713" TEXT="can become tense, with animosity"/>
+<node CREATED="1433169586221" ID="ID_925574298" MODIFIED="1433169589681" TEXT="no communication"/>
+</node>
+</node>
+<node CREATED="1433169811644" ID="ID_1493679615" MODIFIED="1433169824450">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Primary goal:&#160;<b>Produce</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1433169843595" ID="ID_1365377580" MODIFIED="1433171472962" TEXT="juniors with seniors">
+<icon BUILTIN="full-4"/>
+<node CREATED="1433169956226" ID="ID_1276890154" MODIFIED="1433169967086" TEXT="requires a different approach"/>
+<node CREATED="1433170005138" ID="ID_55836544" MODIFIED="1433170008886" TEXT="happens when">
+<node CREATED="1433169969162" ID="ID_1694058850" MODIFIED="1433170004374" TEXT="new devs join the company"/>
+<node CREATED="1433170018578" ID="ID_1577335190" MODIFIED="1433170030366" TEXT="devs switch to a new development context"/>
+</node>
+<node CREATED="1433170041850" ID="ID_418673056" MODIFIED="1433170083669" TEXT="expectations">
+<node CREATED="1433170049034" ID="ID_275973684" MODIFIED="1433170126885" TEXT="leadership must understand that the &quot;junior&quot; will be ramped up as fast as possible"/>
+<node CREATED="1433170183137" ID="ID_66523612" MODIFIED="1433170191821" TEXT="every one must understand this">
+<node CREATED="1433170196097" ID="ID_970706029" MODIFIED="1433170196925" TEXT="QA"/>
+<node CREATED="1433170197649" ID="ID_90516760" MODIFIED="1433170203533" TEXT="other devs"/>
+<node CREATED="1433170204217" ID="ID_398551289" MODIFIED="1433170208661" TEXT="designers"/>
+<node CREATED="1433170214945" ID="ID_417950858" MODIFIED="1433171495372" TEXT="especially the senior in the pair">
+<font BOLD="true" NAME="Source Code Pro" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1433170128593" ID="ID_229736631" MODIFIED="1433170167029" TEXT="no pressure to deliver"/>
+</node>
+<node COLOR="#000000" CREATED="1433170240801" ID="ID_815564993" MODIFIED="1433171466752">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      the senior
+    </p>
+    <p style="text-align: center">
+      must mentor
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="yes"/>
+<node CREATED="1433170380616" ID="ID_1504022115" MODIFIED="1433170823645">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      really hard to do as humans since
+    </p>
+    <p>
+      we tend to do the opposite
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="Source Code Pro" SIZE="12"/>
+</node>
+<node CREATED="1433170334008" ID="ID_581233351" MODIFIED="1433170820261" TEXT="helpful">
+<font BOLD="true" NAME="Source Code Pro" SIZE="12"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1433170344928" ID="ID_176887252" MODIFIED="1433170351372" TEXT="listen"/>
+<node CREATED="1433170354472" ID="ID_1691890555" MODIFIED="1433170357468" TEXT="answer questions"/>
+<node CREATED="1433170360440" ID="ID_123506500" MODIFIED="1433170362708" TEXT="observe"/>
+<node CREATED="1433170363384" ID="ID_497805024" MODIFIED="1433170371044" TEXT="ask leading questions"/>
+<node CREATED="1433170652358" ID="ID_1495380769" MODIFIED="1433171312228" STYLE="bubble" TEXT="be patient">
+<font BOLD="true" NAME="Source Code Pro" SIZE="12"/>
+</node>
+<node CREATED="1433170797597" ID="ID_863421867" MODIFIED="1433171312227" STYLE="bubble" TEXT="empathy">
+<font BOLD="true" NAME="Source Code Pro" SIZE="12"/>
+</node>
+<node CREATED="1433171245234" ID="ID_1593803082" MODIFIED="1433171312227" STYLE="bubble" TEXT="self confidence">
+<font BOLD="true" NAME="Source Code Pro" SIZE="12"/>
+</node>
+<node CREATED="1433170587086" ID="ID_68180307" MODIFIED="1433170594698" TEXT="let them make the mistake you see coming">
+<node CREATED="1433170605238" ID="ID_141236930" MODIFIED="1433170612506" TEXT="and let them feel some of the consequences"/>
+</node>
+<node CREATED="1433170621150" ID="ID_1305808056" MODIFIED="1433170627794" TEXT="let them think about why that was bad"/>
+<node CREATED="1433170634318" ID="ID_427018629" MODIFIED="1433170638314" TEXT="let them correct it"/>
+<node CREATED="1433170643006" ID="ID_1453063275" MODIFIED="1433170649466" TEXT="let them ask you questions along the way"/>
+<node CREATED="1433170664934" ID="ID_1433252343" MODIFIED="1433170726972">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      try to answer with <b>questions</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="Source Code Pro" SIZE="12"/>
+</node>
+<node CREATED="1433170697078" ID="ID_1531057331" MODIFIED="1433170703778" TEXT="if they get stuck">
+<node CREATED="1433170704230" ID="ID_738066731" MODIFIED="1433170735555">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      lead them out with <b>questions</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1433170449303" ID="ID_805590881" MODIFIED="1433170821965">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      not helpful
+    </p>
+  </body>
+</html>
+</richcontent>
+<font BOLD="true" NAME="Source Code Pro" SIZE="12"/>
+<icon BUILTIN="closed"/>
+<node CREATED="1433170460727" ID="ID_2332868" MODIFIED="1433171770224" STYLE="bubble" TEXT="DO NOT tell them what to type"/>
+<node CREATED="1433170515375" ID="ID_297824032" MODIFIED="1433171770225" STYLE="bubble" TEXT="DO NOT tell them what to do"/>
+<node CREATED="1433170538271" ID="ID_1506498863" MODIFIED="1433171770225" STYLE="bubble" TEXT="DO NOT take over and show them"/>
+</node>
+<node CREATED="1433171046651" ID="ID_1380452361" MODIFIED="1433171048511" TEXT="Goals">
+<node CREATED="1433170852413" ID="ID_854952674" MODIFIED="1433170873049" TEXT="we want to build the junior up, not show him what you know"/>
+<node CREATED="1433171059059" ID="ID_127912159" MODIFIED="1433171209403" TEXT="Mentoring and Learning">
+<font BOLD="true" NAME="Source Code Pro" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1433171011587" ID="ID_231792814" MODIFIED="1433171015983" TEXT="the ugly">
+<node CREATED="1433170968692" ID="ID_1038244032" MODIFIED="1433171005959" TEXT="everyone is busy trying to get stuff done"/>
+<node CREATED="1433171024091" ID="ID_475721444" MODIFIED="1433171036687" TEXT="don&apos;t have human skills to do it well"/>
+</node>
+<node CREATED="1433171074675" ID="ID_1681290978" MODIFIED="1433171093706">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Questions you may
+    </p>
+    <p>
+      ask yourself
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1433171096459" ID="ID_1620498550" MODIFIED="1433171120831" TEXT="Do they share knowledge?"/>
+<node CREATED="1433171111467" ID="ID_1354413166" MODIFIED="1433171122031" TEXT="Do they help juniors often?"/>
+<node CREATED="1433171123715" ID="ID_597088355" MODIFIED="1433171154606" TEXT="Do they get defensive about their?">
+<node CREATED="1433171143363" ID="ID_717723109" MODIFIED="1433171145303" TEXT="skills"/>
+<node CREATED="1433171146115" ID="ID_747955389" MODIFIED="1433171150462" TEXT="opinions"/>
+</node>
+<node CREATED="1433171161610" ID="ID_836799554" MODIFIED="1433171191190" TEXT="Are they comfortable with sacrificing for the good of the team?"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1433171543200" ID="ID_952727527" MODIFIED="1433171545396" POSITION="left" TEXT="Culture">
+<node CREATED="1433171576783" ID="ID_790114986" MODIFIED="1433171635155" TEXT="good things happen when there is understanding between people"/>
+<node CREATED="1433171587983" ID="ID_1151800102" MODIFIED="1433171592867" TEXT="Understanding comes from">
+<node CREATED="1433171595967" ID="ID_1287157548" MODIFIED="1433171600443" TEXT="empathy"/>
+<node CREATED="1433171601303" ID="ID_1552684188" MODIFIED="1433171607179" TEXT="communication"/>
+<node CREATED="1433171608351" ID="ID_1198767074" MODIFIED="1433171614395" TEXT="self confidence"/>
+</node>
+<node CREATED="1433171644687" ID="ID_1497140100" MODIFIED="1433171647739" TEXT="comes from the top"/>
+<node CREATED="1433171652215" ID="ID_1923179055" MODIFIED="1433171671603" TEXT="it&apos;s not what they say, it&apos;s how they act">
+<font BOLD="true" NAME="Source Code Pro" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1433171704415" ID="ID_1608396120" MODIFIED="1433171978952" POSITION="right" TEXT="Conclusions">
+<node CREATED="1433171712254" ID="ID_560427222" MODIFIED="1433171730386" TEXT="programming is as much about people">
+<node CREATED="1433171732054" ID="ID_1196687782" MODIFIED="1433171737458" TEXT="as it is about technology"/>
+</node>
+<node CREATED="1433171825886" ID="ID_1976714165" MODIFIED="1433171841433" TEXT="we are people">
+<node CREATED="1433171841958" ID="ID_222576303" MODIFIED="1433171891849" TEXT="writing software">
+<node CREATED="1433171894509" ID="ID_197399066" MODIFIED="1433171895345" TEXT="in colaboration with other people"/>
+<node CREATED="1433171896189" ID="ID_1003369987" MODIFIED="1433171898337" TEXT="for people"/>
+</node>
+</node>
+<node CREATED="1433171923461" ID="ID_68905020" MODIFIED="1433171957717" TEXT="great programmer">
+<font BOLD="true" NAME="Source Code Pro" SIZE="12"/>
+<node CREATED="1433171906413" ID="ID_1706675347" MODIFIED="1433171947937" TEXT="great tech skills"/>
+<node CREATED="1433171948653" ID="ID_818020472" MODIFIED="1433171956441" TEXT="being a great person"/>
+</node>
+</node>
+</node>
+</map>
---
+++ Craftsmanhip/Peer-code-review/Peer-code-review.mm
@@ -0,0 +1,19 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1433157563842" ID="ID_944767025" LINK="https://blog.salsitasoft.com/practical-lessons-in-peer-code-review/" MODIFIED="1433158564878">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      Peer code
+    </p>
+    <p style="text-align: center">
+      review
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</map>
