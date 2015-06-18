3959859e37dfda2becaddb196f06ca8bc52fad76
Yoyosan <veselu@gmail.com>
2015-06-18 08:45:47 +0300

    Add Nutrition made clear. Ai

---
+++ Nutrition made clear/Nutrition-made-clear.mm
@@ -0,0 +1,122 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1434603785045" ID="ID_1079687324" MODIFIED="1434603813178">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Nutrition
+    </p>
+    <p>
+      made clear
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1434603815492" ID="ID_1719866592" MODIFIED="1434603874316" POSITION="right" TEXT="Why we eat what we do">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1434603843769" ID="ID_1366239444" MODIFIED="1434603876310" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sources of nutrition
+    </p>
+    <p>
+      Fact and fiction
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="full-2"/>
+<node CREATED="1434603880281" ID="ID_555430224" MODIFIED="1434604841934" TEXT="fibers are the colon cleansing mechanism nature gave us"/>
+<node CREATED="1434603922393" ID="ID_1946556150" MODIFIED="1434604729705" TEXT="Avoiding fraud">
+<node CREATED="1434604055879" ID="ID_27207933" MODIFIED="1434604068363" TEXT="check www.fda.gov"/>
+<node CREATED="1434604127151" ID="ID_1549737212" MODIFIED="1434604134011" TEXT="Is it a quick and effective cure-all?"/>
+<node CREATED="1434604191959" ID="ID_74008451" MODIFIED="1434604214426" TEXT="Can it treat a serious or incurable disease?"/>
+<node CREATED="1434604436013" ID="ID_1304387238" MODIFIED="1434604438313" TEXT="Claims of">
+<node CREATED="1434604438917" ID="ID_1671445966" MODIFIED="1434604465353" TEXT="miraculous breakthrough"/>
+<node CREATED="1434604466404" ID="ID_1781485119" MODIFIED="1434604473488" TEXT="secret formula"/>
+<node CREATED="1434604474316" ID="ID_748500894" MODIFIED="1434604497011">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      hidden ingredient your doctor
+    </p>
+    <p>
+      doesn't want you to know about
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434604518860" ID="ID_1828435266" MODIFIED="1434604529434" TEXT="useless medical terminology"/>
+</node>
+<node CREATED="1434604536364" ID="ID_1140089908" MODIFIED="1434604557676">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      all natural = all safe
+    </p>
+    <p>
+      which is false!
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1434604622851" ID="ID_856650461" MODIFIED="1434604643999" TEXT="personal testimonies claiming amazing results"/>
+<node CREATED="1434604670972" ID="ID_1591797407" MODIFIED="1434604675735" TEXT="before and after pictures"/>
+<node CREATED="1434604691867" ID="ID_608688219" MODIFIED="1434604701063" TEXT="limited availability, act now!"/>
+<node CREATED="1434604730787" ID="ID_230628285" MODIFIED="1434604778622" TEXT="No-risk or Money-back guarantees"/>
+<node CREATED="1434604780026" ID="ID_732469016" MODIFIED="1434604801070" TEXT="Easy fix for weight loss or impotency"/>
+</node>
+<node CREATED="1434604650523" HGAP="21" ID="ID_1334058121" MODIFIED="1434604976629" TEXT="Credible sources" VSHIFT="9">
+<node CREATED="1434604927466" ID="ID_1986691069" MODIFIED="1434604934869" TEXT="www.eatright.org">
+<node CREATED="1434604966129" ID="ID_443406985" MODIFIED="1434604972509" TEXT="has diet books review"/>
+</node>
+<node CREATED="1434605017961" ID="ID_983263448" MODIFIED="1434605021605" TEXT="www.acsm.org">
+<node CREATED="1434605029649" ID="ID_1119262799" MODIFIED="1434605037733" TEXT="premier fitness organization"/>
+<node CREATED="1434605038537" ID="ID_1050466345" MODIFIED="1434605113752">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      check info about a trend your
+    </p>
+    <p>
+      trainer tells you about
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1434605064641" ID="ID_181359000" MODIFIED="1434605077452" TEXT="www.cdc.gov">
+<node CREATED="1434605097840" ID="ID_129552902" MODIFIED="1434605106181" TEXT="solid info about diseases"/>
+<node CREATED="1434605130176" ID="ID_741779360" MODIFIED="1434605135740" TEXT="BMI calculator"/>
+</node>
+<node CREATED="1434605161272" ID="ID_406595196" MODIFIED="1434605164812" TEXT="www.fda.gov">
+<node CREATED="1434605165448" ID="ID_1423230982" MODIFIED="1434605170828" TEXT="info about recalled food"/>
+<node CREATED="1434605174984" ID="ID_1688641181" MODIFIED="1434605179020" TEXT="consumer warnings"/>
+</node>
+<node CREATED="1434605202240" ID="ID_850218042" MODIFIED="1434605213180" TEXT="www.fnic.nal.usda.gov">
+<node CREATED="1434605251551" ID="ID_1509897513" MODIFIED="1434605298451" TEXT="US agriculture website about food and nutrition"/>
+</node>
+</node>
+</node>
+</node>
+</map>
